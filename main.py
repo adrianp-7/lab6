089037980fbf1b59088b85ab86883c50d16a7f8b
Angela Luca <tennisangel22@outlook.com>
2023-10-31 16:24:53 -0400

    Added decoder function, option 2, option 3

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def main():
         if menu_selec == "1":
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
-            password = encode(password)
+            new_password = encode(password)  # changed variable name to avoid confusion
+        elif menu_selec == "2":
+            print("The encoded password is", new_password, "and the original password is", decoder(new_password))
+        elif menu_selec == "3":
+            break
 
 def encode(password):
     new_pass = ""
@@ -17,6 +21,19 @@ def encode(password):
         else:
             new_pass += str(int(dig) - 7)
     return new_pass
+
+
+def decoder(password):
+    pass_list = []
+    # change each string character to int, then back to str, append to list
+    for i in password:
+        if int(i) < 3:
+            pass_list.append(str(int(i) + 7)) # if digit is 2, then returns 12-3=9, not 2-3=-1
+        else:
+            pass_list.append(str(int(i) - 3))
+    # change list to string without whitespace
+    pass_string = "".join(pass_list)
+    return pass_string
 
 
 if __name__ == "__main__":
